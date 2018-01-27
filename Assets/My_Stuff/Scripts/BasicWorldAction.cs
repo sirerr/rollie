56b210dc4cfff4342c568d32a5915f916973f6d5
KCTCSACC\bl-ggj-guser02 <ggjgreatdesigns@gmail.com>
2018-01-27 16:33:41 -0500

    2D stuff

---
+++ Assets/My_Stuff/Scripts/BasicWorldAction.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicWorldAction : MonoBehaviour {
+
+	public virtual void InteractStart()
+	{
+	}
+
+	public virtual void InteractStop()
+	{
+	}
+}
---
+++ Assets/My_Stuff/Scripts/GearsRotateTest_1.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GearsRotateTest_1 : BasicWorldAction {
+
+	public List<Transform> GearsSame = new List<Transform>();
+	public List<Transform> GearsOpposite = new List<Transform>();
+
+	private bool rotating = false;
+
+
+	// Update is called once per frame
+	void Update ()
+	{
+//		if (Input.GetMouseButtonDown(0))
+//		{
+//			Vector3 = wp = Camera.main.ScreenToWorldPoint (Input.mousePosition);
+//			PolygonCollider2D coll =
+
+		if (rotating) {
+			this.transform.Rotate (0, 0, -1);
+			for (int i = 0; i < GearsSame.Count; i++)
+			{
+				GearsSame[i].Rotate (0, 0, -1);
+			}
+			for (int i = 0; i < GearsOpposite.Count; i++)
+			{
+				GearsOpposite[i].Rotate (0, 0, 1);
+			}
+		}
+	}
+
+	public override void InteractStart()
+	{
+		rotating = true;
+	}
+
+	public override void InteractStop()
+	{
+		rotating = false;
+	}
+
+	#if UNITY_EDITOR
+	void OnMouseUp()
+	{
+		InteractStop();
+	}
+
+	void OnMouseDown()
+	{
+		InteractStart();
+	}
+	#endif
+}
