2c9270cddc98f609ed3b9c90a2aefe1b885e54f1
KCTCSACC\bl-ggj-guser02 <ggjgreatdesigns@gmail.com>
2018-01-28 13:03:28 -0500

    fixed mothership and added new color toggle

---
+++ Assets/My_Stuff/Scripts/LightMoveUp.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightMoveUp : BasicWorldAction {
+
+
+	public bool moveup = false;
+	public bool levertrig = false;
+	public bool trigger2 = false;
+	public Animator anim;
+	public float YSpeed = .3f;
+	float startY;
+	float MaxDistance = .17f;
+	void Start()
+
+
+	{
+		startY = transform.position.y;
+	
+	}
+
+
+	// Update is called once per frame
+	void Update ()
+	{
+		//		if (Input.GetMouseButtonDown(0))
+		//		{
+		//			Vector3 = wp = Camera.main.ScreenToWorldPoint (Input.mousePosition);
+		//			PolygonCollider2D coll =
+
+		if (moveup && transform.position.y < (startY + MaxDistance)) {
+			this.transform.Translate (0, YSpeed * Time.deltaTime, 0);
+							
+
+			//rigidbody.MoveRotation(rigidbody.rotation - 0.5f);
+		} else if(moveup) {
+			levertrig = true;
+		}
+		if (levertrig == true && trigger2 == false)
+		{
+			print ("disguy");
+			anim.SetBool ("lightturn", true);
+			trigger2 = true;
+		}
+	}
+
+
+
+	public override void InteractStart()
+	{
+		moveup = true;
+	}
+
+	public override void InteractStop()
+	{
+		moveup = false;
+	}
+
+	#if UNITY_EDITOR
+	void OnMouseUp()
+	{
+		InteractStop();
+	}
+
+	void OnMouseDown()
+	{
+		InteractStart();
+	}
+	#endif
+}
