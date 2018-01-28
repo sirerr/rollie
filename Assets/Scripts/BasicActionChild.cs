948c505e00c15a78c56f909ae6440cf4daeb964e
leonardw <sirerr@gmail.com>
2018-01-27 21:00:51 -0500

    interaction stuff working

--- Assets/Scripts/BasicActionChild.cs
+++ Assets/Scripts/BasicActionChild.cs
@@ -4,6 +4,19 @@ using UnityEngine;
 
 public class BasicActionChild : BasicWorldAction {
 
- 
+    public float speed =2f;
+
+    public override void InteractStart()
+    {
+        base.InteractStart();
+        transform.Translate(transform.up * speed * Time.deltaTime);
+
+    }
+
+    public override void InteractStop()
+    {
+        base.InteractStop();
+        continuous = false;
+    }
 
 }
