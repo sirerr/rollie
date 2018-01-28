b44d6da238a8a44804e8286cf4896c9f5ba235b4
KCTCSACC\bl-ggj-guser02 <ggjgreatdesigns@gmail.com>
2018-01-27 20:37:44 -0500

    World Creation and Extra Assets...

---
+++ Assets/My_Stuff/Scripts/TrapDoorOpen.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrapDoorOpen : BasicWorldAction {
+
+	public Animator anim;
+
+	// Use this for initialization
+	void Start () {
+		anim = GetComponent<Animator> ();
+	}
+
+	public override void InteractStart()
+	{
+		anim.SetBool ("interact", true);
+	}
+
+	// Update is called once per frame
+	void Update () {
+ 
+	}
+
+	#if UNITY_EDITOR
+	void OnMouseUp()
+	{
+		InteractStop();
+	}
+
+	void OnMouseDown()
+	{
+		InteractStart();
+	}
+	#endif
+}
