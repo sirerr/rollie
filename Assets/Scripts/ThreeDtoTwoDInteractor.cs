23d4af6f7b0e33fbffd89fe89a09b0e183a41900
leonardw <sirerr@gmail.com>
2018-01-28 00:32:59 -0500

    it's working so I'm happy

---
+++ Assets/Scripts/ThreeDtoTwoDInteractor.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThreeDtoTwoDInteractor : BasicWorldAction {
+
+    public override void InteractStart()
+    {
+        transform.parent.GetComponent<BasicWorldAction>().InteractStart();
+    }
+
+    public override void InteractStop()
+    {
+        transform.parent.GetComponent<BasicWorldAction>().InteractStop();
+    }
+}
